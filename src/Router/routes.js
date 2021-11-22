20b3d04679657890d7fdeb8528b3373813d723c6
brandiniA <90206743+brandiniA@users.noreply.github.com>
2021-11-22 15:42:45 -0600

    Router Constants update

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,8 +1,8 @@
 import { Route } from 'react-router-dom'
 // import PrivateRoute from "./CustomRoutes/PrivateRoute"
-import PublicRoute from './CustomRoutes/PublicRoute'
+// import PublicRoute from './CustomRoutes/PublicRoute'
 import * as paths from '../constants/routerConstants'
-import SignInLayout from './../components/Layouts/SignInLayout'
+// import SignInLayout from './../components/Layouts/SignInLayout'
 import MarketLayout from '../components/Layouts/MarketLayout'
 
 const routes = [
@@ -15,36 +15,36 @@ const routes = [
     //     routes: []
     //     layout: NoAuthLayout
     // },
-    {
-        name: 'LandingPage',
-        path: paths.HomePath,
-        route: Route,
-        componentProps: { routes: [] },
-        exact: true,
-    },
-    {
-        name: 'AuthView',
-        path: paths.LoginInPath,
-        route: PublicRoute,
-        componentProps: {
-            auth: 'login',
-            routes: [],
-        },
-        layout: SignInLayout,
-    },
-    {
-        name: 'AuthView',
-        path: paths.SignInPath,
-        route: PublicRoute,
-        componentProps: {
-            auth: 'signin',
-            routes: [],
-        },
-        layout: SignInLayout,
-    },
+    // {
+    //     name: 'LandingPage',
+    //     path: paths.HomePath,
+    //     route: Route,
+    //     componentProps: { routes: [] },
+    //     exact: true,
+    // },
+    // {
+    //     name: 'AuthView',
+    //     path: paths.LoginInPath,
+    //     route: PublicRoute,
+    //     componentProps: {
+    //         auth: 'login',
+    //         routes: [],
+    //     },
+    //     layout: SignInLayout,
+    // },
+    // {
+    //     name: 'AuthView',
+    //     path: paths.SignInPath,
+    //     route: PublicRoute,
+    //     componentProps: {
+    //         auth: 'signin',
+    //         routes: [],
+    //     },
+    //     layout: SignInLayout,
+    // },
     {
         name: 'MarketView',
-        path: paths.MarketPath,
+        path: paths.HomePath,
         route: Route,
         componentProps: {
             routes: [],
--- src/constants/routerConstants.js
+++ src/constants/routerConstants.js
@@ -5,7 +5,7 @@ export const LoginInPath = `${RootPath}/login`
 export const SignInPath = `${RootPath}/signup`
 export const LogOutPath = `${RootPath}/logout`
 export const TravelPath = `${RootPath}/travel`
-export const MarketPath = `${RootPath}/market`
+export const MarketPath = `${RootPath}/`
 
 export const tripvixiaURL =
     'https://members.tripvixia.com/membership/signup?invite=0e3cb515-2949-4e1a-bb83-fd7fe4a5b251&referraltype=3'
