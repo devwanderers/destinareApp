bca014ae29739f195219d81d68051034f76014d7
Alvaro Monrreal <alvaro.monrreal@gmail.com>
2022-01-11 11:33:24 -0600

    hide staking

--- src/components/Layouts/Siders/SiderMarket.js
+++ src/components/Layouts/Siders/SiderMarket.js
@@ -11,11 +11,11 @@ import {
 import { FaHome } from 'react-icons/fa'
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
 import { MdDashboard } from 'react-icons/md'
-import { GiWarPick } from 'react-icons/gi'
+// import { GiWarPick } from 'react-icons/gi'
 import {
     HomePath,
     MarketPath,
-    StakingView,
+    // StakingView,
 } from '../../../constants/routerConstants'
 
 const { Sider } = Layout
@@ -35,19 +35,19 @@ const menuItems = [
         onClick: MarketPath,
         group: false,
     },
-    {
-        icon: GiWarPick,
-        key: 'earn',
-        name: 'Earn',
-        group: true,
-        items: [
-            {
-                onClick: StakingView,
-                key: 'staking',
-                name: 'Staking with lock',
-            },
-        ],
-    },
+    // {
+    //     icon: GiWarPick,
+    //     key: 'earn',
+    //     name: 'Earn',
+    //     group: true,
+    //     items: [
+    //         {
+    //             onClick: StakingView,
+    //             key: 'staking',
+    //             name: 'Staking with lock',
+    //         },
+    //     ],
+    // },
 ]
 
 const SiderMarket = ({ collapsed, onCollapse, menuKey }) => {
