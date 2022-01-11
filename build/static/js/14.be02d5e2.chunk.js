f18eba9be3ec5cc62ca687b9fa2ce15171b3fe05
Alvaro Monrreal <alvaro.monrreal@gmail.com>
2022-01-11 11:44:59 -0600

    add build

---
+++ build/static/js/14.be02d5e2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpswapp=this.webpackJsonpswapp||[]).push([[14],{500:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));var a=e(6),c=e(7),u=e(10),r=e(11),i=e(1),o=e(13),s=function(t){Object(u.a)(e,t);var n=Object(r.a)(e);function e(t){var c;return Object(a.a)(this,e),(c=n.call(this,t)).state={},c}return Object(c.a)(e,[{key:"componentDidMount",value:function(){}},{key:"render",value:function(){return Object(o.jsx)("div",{})}}]),e}(i.Component)}}]);
+//# sourceMappingURL=14.be02d5e2.chunk.js.map
