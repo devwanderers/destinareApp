a45ff1776b416759ff13b21f8250958bc7c6d8b7
Alvaro Monrreal <alvaro.monrreal@gmail.com>
2022-01-18 12:38:54 -0600

    main build

---
+++ build/static/js/15.b67aeda8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpswapp=this.webpackJsonpswapp||[]).push([[15],{504:function(e,s,c){"use strict";c.r(s);c(1);var t=c(13),n=function(){return Object(t.jsx)("div",{className:"css-generic w-full min-h-screen justify-center items-center",children:Object(t.jsx)("div",{className:"css-generic text-gray-9 mx-auto text-8xl",children:"Error 404"})})};s.default=function(e){return Object(t.jsx)("div",{className:"css-generic w-full min-h-screen justify-center items-center",children:Object(t.jsx)(n,{})})}}}]);
+//# sourceMappingURL=15.b67aeda8.chunk.js.map
