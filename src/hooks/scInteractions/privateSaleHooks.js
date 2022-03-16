9b7fbd7a93f89d1547583627fb9502c896fab02a
brandiniA <90206743+brandiniA@users.noreply.github.com>
2022-03-16 17:01:02 -0600

    Hooks

--- src/hooks/scInteractions/privateSaleHooks.js
+++ src/hooks/scInteractions/privateSaleHooks.js
@@ -5,22 +5,40 @@ import DestinareContract from '../../abi/DestinareContract.json'
 export const useClaimPrivateSale = () => {
     const { library, account } = useActiveWeb3React()
 
-    return useCallback(async (callback) => {
-        try {
-            const contract = new library.eth.Contract(
-                DestinareContract,
-                process.env.REACT_APP_DESTINARE_CONTRACT_ADDRESS
-            )
-        } catch (err) {
-            console.log({ err })
-            if (typeof callback === 'function') {
-                const _err = { err }
-                callback(_err)
+    return useCallback(
+        async (callback) => {
+            try {
+                const contract = new library.eth.Contract(
+                    DestinareContract,
+                    process.env.REACT_APP_DESTINARE_CONTRACT_ADDRESS
+                )
+            } catch (err) {
+                if (typeof callback === 'function') {
+                    const _err = { err }
+                    callback(_err)
+                }
             }
-        }
-    })
+        },
+        [library, account]
+    )
 }
 
 export const useBuyPrivateSale = () => {
-    return useCallback(async () => {})
+    const { library, account } = useActiveWeb3React()
+    return useCallback(
+        async (callback) => {
+            try {
+                const contract = new library.eth.Contract(
+                    DestinareContract,
+                    process.env.REACT_APP_DESTINARE_CONTRACT_ADDRESS
+                )
+            } catch (err) {
+                if (typeof callback === 'function') {
+                    const _err = { err }
+                    callback(_err)
+                }
+            }
+        },
+        [library, account]
+    )
 }
